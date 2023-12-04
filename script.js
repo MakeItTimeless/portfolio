d9f37a8b7f8a20fb78c25c7f208f96c5160de85a
MakeItTimeless <sryjni@gmail.com>
2023-12-04 18:30:10 +0530

    sticky_issue

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const header = document.querySelector("header");
 window.addEventListener('scroll',function(){
     header.classList.toggle("sticky", window.scroLLy > 120);
 })
+
+let menu = documnet.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+}
